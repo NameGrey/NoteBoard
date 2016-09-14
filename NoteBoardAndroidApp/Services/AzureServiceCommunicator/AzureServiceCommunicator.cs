a91a29aa11c687d40b5f53ac82b00069025869b2
Timokhin, Sergey <Sergey.Timokhin@Kronos.com>
2016-09-14 16:08:03 +0300

    Implement Azure service communicator

--- NoteBoardAndroidApp/Services/AzureServiceCommunicator/AzureServiceCommunicator.cs
+++ NoteBoardAndroidApp/Services/AzureServiceCommunicator/AzureServiceCommunicator.cs
@@ -1,13 +1,43 @@
 using System;
+using System.IO;
+using System.Net;
 
 namespace NoteBoardAndroidApp.Services.AzureServiceCommunicator
 {
-	public class AzureServiceCommunicator:IAzureServiceCommunicator
+	public class AzureServiceCommunicator: IAzureServiceCommunicator
 	{
-		public AzureResponse SendRequest(string data, string uri)
+		public AzureResponse SendRequest(string data, string uri, string method)
 		{
-			// TODO: Use WebClient to send request
-			throw new NotImplementedException();
+			AzureResponse result = new AzureResponse();
+			HttpWebRequest request = WebRequest.CreateHttp(uri);
+			request.ContentType = "application/json";
+			request.Method = method;
+
+			try
+			{
+				result = FillInAzureResponse(request.GetResponse());
+			}
+			catch (WebException ex)
+			{
+				result.ErrorMessage = ex.Message;
+				result.Status = (int) ((HttpWebResponse) ex.Response).StatusCode;
+			}
+
+			return result;
+		}
+
+		private AzureResponse FillInAzureResponse(WebResponse response)
+		{
+			AzureResponse result = new AzureResponse();
+
+			using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+			{
+				result.Data = reader.ReadToEnd();
+				result.Status = (int) ((HttpWebResponse) response).StatusCode;
+				result.ErrorMessage = String.Empty;
+			}
+
+			return result;
 		}
 	}
 }
