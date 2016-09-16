c7c49b637b12d722da5ccc015d3d80a4d241a71a
Timokhin, Sergey <Sergey.Timokhin@Kronos.com>
2016-09-16 10:19:31 +0300

    Add NoteController

---
+++ AzureNoteService/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AzureNoteService.Controllers
+{
+	[RoutePrefix("api")]
+	public class NoteController : ApiController
+	{
+		[Route("notes")]
+		public string GetCollection()
+		{
+			return "Hello World";
+		}
+
+	}
+}
