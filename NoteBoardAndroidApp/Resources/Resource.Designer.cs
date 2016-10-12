89fbc2c7e703f024bf29db131c418013bfe2e3ea
Timokhin, Sergey <Sergey.Timokhin@Kronos.com>
2016-10-12 18:16:50 +0300

    Change microphone button

--- NoteBoardAndroidApp/Resources/Resource.Designer.cs
+++ NoteBoardAndroidApp/Resources/Resource.Designer.cs
@@ -45,19 +45,16 @@ namespace NoteBoardAndroidApp
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Arrow = 2130837504;
+			public const int Arrow_50h = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int Arrow_50 = 2130837505;
+			public const int Icon = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int Arrow_50h = 2130837506;
+			public const int microphoneNotActive = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int Icon = 2130837507;
-			
-			// aapt resource value: 0x7f020004
-			public const int microphoneNotActive = 2130837508;
+			public const int microphoneNotActive_up = 2130837507;
 			
 			static Drawable()
 			{
