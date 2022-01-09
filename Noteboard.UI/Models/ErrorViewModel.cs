5e1f912c41c399b8bbb82be79248ddb59151f5b0
Sergey Timokhin <sergey_timokhin@outlook.com>
2022-01-09 13:57:18 +0300

    Create new ASP NET MVC UI application for noteboard

---
+++ Noteboard.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Noteboard.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
