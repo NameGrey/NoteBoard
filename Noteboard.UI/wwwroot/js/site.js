91daea2de5e1335f66b75df2f7a3f4f280b543e2
Sergey Timokhin <sergey_timokhin@outlook.com>
2022-01-10 13:56:49 +0300

    Add picture with microfon and use Web API for voice recognition

--- Noteboard.UI/wwwroot/js/site.js
+++ Noteboard.UI/wwwroot/js/site.js
@@ -1,4 +1,34 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+var recognition = new SpeechRecognition();
 
-// Write your JavaScript code.
+recognition.continuous = false;
+recognition.lang = 'ru-RU';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+recognition.onresult = function (event) {
+    var word = event.results[0][0].transcript;
+    console.log('Result received: ' + word + '.');
+}
+
+recognition.onspeechend = function () {
+    recognition.stop();
+}
+
+recognition.onnomatch = function (event) {
+    console.log("I didn't recognise that word.");
+}
+
+recognition.onerror = function (event) {
+    console.log('Error occurred in recognition: ' + event.error);
+}
+
+var micButton = document.getElementById("mic-button");
+
+if (micButton) {
+    micButton.onclick = function() {
+        recognition.start();
+        console.log('Start recognition.');
+    }
+}
